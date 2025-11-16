88b35d74dfb6be4ab6e3d73d8ad642f3d7f4e77c
tjtxn <tom@tunstill.dev>
2025-11-16 22:42:54 +0000

    CH2, L3: added GUI

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from logger import log_state
+
+pygame.init()
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 def main():
-    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
-    print(f"""Screen width: {SCREEN_WIDTH}
+	print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+	print(f"""Screen width: {SCREEN_WIDTH}
 Screen height: {SCREEN_HEIGHT}""")
+
+	while 1 == 1:
+		log_state()
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				return
+		screen.fill("black")
+		pygame.display.flip()
+
 
 
 if __name__ == "__main__":
     main()
+
