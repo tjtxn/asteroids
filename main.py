70e97b6cc6141af4b5ab7e4f9bdc1fe39905f5ef
tjtxn <tom@tunstill.dev>
2025-11-16 21:48:04 +0000

    CH2, L1: project setup and module

---
+++ main.py
@@ -0,0 +1,11 @@
+import pygame
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+
+def main():
+    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    print(f"""Screen width: {SCREEN_WIDTH}
+Screen height: {SCREEN_HEIGHT}""")
+
+
+if __name__ == "__main__":
+    main()
