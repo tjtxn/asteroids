7a79f03292559cfb3b43bd18aaf7c3992e330796
tjtxn <tom@tunstill.dev>
2025-11-17 19:41:24 +0000

    CH2, L4: limited FPS to 60

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
 
 pygame.init()
+clock = pygame.time.Clock()
+dt = 0
 
 def main():
 	print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -17,9 +19,11 @@ Screen height: {SCREEN_HEIGHT}""")
 			if event.type == pygame.QUIT:
 				return
 		screen.fill("black")
-		# be sure to run this last
+
+		# be sure to run this last - exception for dt
 		pygame.display.flip()
 
+		dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
