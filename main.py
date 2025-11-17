a72c76ed50d4c29008cf54e739330718fba2f106
tjtxn <tom@tunstill.dev>
2025-11-17 20:40:08 +0000

    CH3, L2: drawing the player

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 pygame.init()
 clock = pygame.time.Clock()
@@ -13,6 +14,8 @@ Screen height: {SCREEN_HEIGHT}""")
 
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
 	while 1 == 1:
 		log_state()
 		for event in pygame.event.get():
@@ -20,10 +23,13 @@ Screen height: {SCREEN_HEIGHT}""")
 				return
 		screen.fill("black")
 
+		player.draw(screen)
+
 		# be sure to run this last - exception for dt
 		pygame.display.flip()
 
 		dt = clock.tick(60) / 1000
+		
 
 
 if __name__ == "__main__":
